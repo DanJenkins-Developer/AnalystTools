81bd26e378ae85ba7c574f5bab9b63b4fbf9f385
DanJenkins-Developer <djenkinsjg0617@gmail.com>
2025-05-16 15:39:31 -0500

    Initial files

---
+++ decode.py
@@ -0,0 +1,31 @@
+def loadPSCommandsFromFile():
+    with open('normalized-command-lines.txt') as file:
+        commands = file.read().splitlines()
+        return commands
+    
+def getEncodedCommandArgumentPosition(command: str):
+
+    position = 6 # change
+    arguments = command.split(" ")
+    for argument in arguments:
+        print(argument)
+    return position
+    
+
+def getEncodedCommand(command: str, position: int = 6):
+
+    arguments = command.split(" ")
+    encodedCommand = arguments[position]
+    return encodedCommand
+
+
+PSCommandsText = loadPSCommandsFromFile()
+decodedCommands = []
+
+for command in PSCommandsText:
+    getEncodedCommand(command)
+    decodedCommands.append(getEncodedCommand(command))
+
+
+
+
